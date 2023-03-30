4452bd56b3b6f7c648c544e16b3037b7991b1e19
Bwaweru12345 <111865501+Bwaweru12345@users.noreply.github.com>
2023-03-30 13:03:32 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
+  const input = document.getElementById('input');
 
 function clickAlert() {
-  alert('I was clicked!');
+alert('I was clicked!');
 }
 
 input.addEventListener('click', clickAlert);
 }
+
+// end of code
